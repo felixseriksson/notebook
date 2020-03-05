8d6ffd5b67a770df54c323cdb041a34818bbe157
Felix Steinberger Eriksson <felix.s.eriksson@gmail.com>
2020-03-05 20:40:37 +0100

    added script to calculate number of lattice points

--- latticepointsinsidecircle.py
+++ latticepointsinsidecircle.py
@@ -1,1 +1,10 @@
-# test comment
+from math import floor
+def latticepoints(r):
+    """returns number of lattice points inside circle with int or float radius r, requires from math import floor"""
+    return 1 + 4*floor(r) + 4*sum([floor(((r**2)-(i**2))**0.5) for i in range(1, floor(r)+1)])
+
+#testing: comment out block comments
+'''
+for n in range(20):
+    print(latticepoints(n))
+'''
