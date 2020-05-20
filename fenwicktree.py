929188502cc1a4212671ad374a2571dc72abaa9b
Felix Steinberger Eriksson <felix.s.eriksson@gmail.com>
2020-05-20 03:59:58 +0200

    added implementation of fenwick tree, credits to from __future__ import solution

---
+++ fenwicktree.py
@@ -0,0 +1,27 @@
+'''
+All credits to from __future__ import solution
+Constructs a fenwicktree of an array. Can update a bit and get the
+sum up to and including i in the array.
+Time Complexity: O(NlogN) for construction, O(logN) for update and query.
+SpaceComplexity: O(N)
+'''
+def fenwicktree(arr):
+    fwtree = [0]*(len(arr)+1)
+    for i in range(len(arr)):
+        updatebit(fwtree,i,arr[i])
+    return fwtree
+
+def updatebit(fwtree,i,val):
+    i += 1
+    while i < len(fwtree):
+        fwtree[i] += val
+        i += i&(-i)
+
+# get sum of [0,i] inclusive
+def getsum(fwtree,i):
+    s = 0
+    i += 1
+    while i > 0:
+        s += fwtree[i]
+        i -= i&(-i)
+    return s
