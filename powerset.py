b6f3b0e980f5fb1ef138d8b7b4f277a544efd90f
Felix Steinberger Eriksson <felix.s.eriksson@gmail.com>
2020-09-14 15:35:41 +0200

    modified powerset to make use of itertools

--- powerset.py
+++ powerset.py
@@ -1,15 +1,24 @@
-def powerset(seq):
-    """
-    Returns all the subsets of this set. This is a generator. Handles lists, not sets.
-    """
-    if len(seq) <= 1:
-        yield seq
-        yield []
-    else:
-        for item in powerset(seq[1:]):
-            yield [seq[0]]+item
-            yield item
+# def powerset(seq):
+#     """
+#     Returns all the subsets of this set. This is a generator. Handles lists, not sets.
+#     """
+#     if len(seq) <= 1:
+#         yield seq
+#         yield []
+#     else:
+#         for item in powerset(seq[1:]):
+#             yield [seq[0]]+item
+#             yield item
+
+# if __name__ == "__main__":
+#     for p in powerset([1, 2, 3, 4]):
+#         print(p)
+
+from itertools import chain, combinations
+def powerset(iterable):
+    s = list(iterable)
+    return chain.from_iterable(combinations(s, r) for r in range(len(s) + 1))
 
 if __name__ == "__main__":
-    for p in powerset([1, 2, 3, 4]):
-        print(p)
+    for ss in powerset(set([1, 2, 3])):
+        print(ss)
